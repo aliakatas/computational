e5b5ba674c0b5647b3d74c3729967207c4647912
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-09-22 11:25:36 +0100

    Adds test for secant method.

--- secant_test/secant_test.cpp
+++ secant_test/secant_test.cpp
@@ -33,46 +33,22 @@ int main() {
 
 	std::cout << "\n  Testing \"secant\" -->\n";
 	std::cout << "  Solving 2*x^2 - 5*x + 1 == 0 \n";
-	/*solf = bisectionf(0.f, 1.f, quadf);
+	solf = secantf(0.f, 1.f, quadf);
 	std::cout << "   Float 1 :: " << solf << ", f(sol) = " << quadf(solf) << "\n";
-	solf = bisectionf(2.f, 3.f, quadf);
+	solf = secantf(2.f, 3.f, quadf);
 	std::cout << "   Float 2 :: " << solf << ", f(sol) = " << quadf(solf) << "\n";
-	sol = bisectiond(0., 1., quadd);
+	sol = secantd(0., 1., quadd);
 	std::cout << "  Double 1 :: " << sol << ", f(sol) = " << quadd(sol) << "\n";
-	sol = bisectiond(2., 3., quadd);
-	std::cout << "  Double 2 :: " << sol << ", f(sol) = " << quadd(sol) << "\n";*/
+	sol = secantd(2., 3., quadd);
+	std::cout << "  Double 2 :: " << sol << ", f(sol) = " << quadd(sol) << "\n";
 	std::cout << " ********************** \n";
 
-	std::cout << "  Solving tan(x) - x == 0 for x in (pi/2, 3*pi/2) and (3*pi/2, 5*pi/2) \n";
-	/*solf = bisectionf(10.f * FLT_EPSILON + M_PI / 2.f, 3.f * M_PI / 2.f - 10.f * FLT_EPSILON, myFuncf);
-	std::cout << "   Float 1 :: " << solf << ", f(sol) = " << myFuncf(solf) << "\n";
-	solf = bisectionf(10.f * FLT_EPSILON + 3.f * M_PI / 2.f, 5.f * M_PI / 2.f - 10.f * FLT_EPSILON, myFuncf);
-	std::cout << "   Float 2 :: " << solf << ", f(sol) = " << myFuncf(solf) << "\n";
-	sol = bisectiond(10. * DBL_EPSILON + M_PI / 2., 3. * M_PI / 2. - 10. * DBL_EPSILON, myFuncd);
-	std::cout << "  Double 1 :: " << sol << ", f(sol) = " << myFuncd(sol) << "\n";
-	sol = bisectiond(10. * DBL_EPSILON + 3. * M_PI / 2., 5. * M_PI / 2. - 10. * DBL_EPSILON, myFuncd);
-	std::cout << "  Double 2 :: " << sol << ", f(sol) = " << myFuncd(sol) << "\n";*/
-	std::cout << " ********************** \n";
-
-	/*std::cout << " ______________________ \n";
-	std::cout << "\n  Testing \"bisection_reg_falsi\" -->\n";
-	std::cout << "  Solving 2*x^2 - 5*x + 1 == 0 \n";*/
-	/*solf = bisectionf_reg_falsi(0.f, 1.f, quadf);
-	std::cout << "   Float 1 :: " << solf << ", f(sol) = " << quadf(solf) << "\n";
-	solf = bisectionf_reg_falsi(2.f, 3.f, quadf);
-	std::cout << "   Float 2 :: " << solf << ", f(sol) = " << quadf(solf) << "\n";
-	sol = bisectiond_reg_falsi(0., 1., quadd);
-	std::cout << "  Double 1 :: " << sol << ", f(sol) = " << quadd(sol) << "\n";
-	sol = bisectiond_reg_falsi(2., 3., quadd);
-	std::cout << "  Double 2 :: " << sol << ", f(sol) = " << quadd(sol) << "\n";*/
-	//std::cout << " ********************** \n";
-
-	//std::cout << "  Solving exp(x) - 8 == 0 \n";
-	/*solf = bisectionf_reg_falsi(0.5f, 3.f, expFuncf);
+	std::cout << "  Solving exp(x) - 8 == 0 \n";
+	solf = secantf(0.5f, 3.f, expFuncf);
 	std::cout << "   Float 1 :: " << solf << ", f(sol) = " << expFuncf(solf) << "\n";
-	sol = bisectiond_reg_falsi(0.5, 3., expFuncd);
-	std::cout << "  Double 1 :: " << sol << ", f(sol) = " << expFuncd(sol) << "\n";*/
-	//std::cout << " ********************** \n";
+	sol = secantd(0.5, 3., expFuncd);
+	std::cout << "  Double 1 :: " << sol << ", f(sol) = " << expFuncd(sol) << "\n";
+	std::cout << " ********************** \n";
 
 	std::cout << "  <-- Done. \n";
 }
