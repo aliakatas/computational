d76407c9595a803d525fbf443e77da335b1de519
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-09-22 11:20:54 +0100

    Adds secant method.

--- secant/secant.cpp
+++ secant/secant.cpp
@@ -1,10 +1,31 @@
 #include "secant.h"
+#include <cmath>
 
-float secantf(float p0, float p1, float (*f)(float), float tol = 1.e-5, int maxIter = 100) {
-
+float secantf(float p0, float p1, float (*f)(float), float tol, int maxIter) {
+	int iter = 0;
+	float temp;
+	while (iter < maxIter) {
+		temp = p1;
+		p1 = p1 - f(p1) * (p1 - p0) / (f(p1) - f(p0));
+		if (fabsf(p0 - p1) < tol)
+			return p1;
+		p0 = temp;
+		++iter;
+	}
+	return p0;
 }
 
-double secantd(double p0, double p1, double (*f)(double), float tol = 1.e-8, int maxIter = 100) {
-
+double secantd(double p0, double p1, double (*f)(double), float tol, int maxIter) {
+	int iter = 0;
+	double temp;
+	while (iter < maxIter) {
+		temp = p1;
+		p1 = p1 - f(p1) * (p1 - p0) / (f(p1) - f(p0));
+		if (fabs(p0 - p1) < tol)
+			return p1;
+		p0 = temp;
+		++iter;
+	}
+	return p0;
 }
 
