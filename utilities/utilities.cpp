515d67e7457488e1b8ad1757b69e3cf6804e829d
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-09-28 11:42:49 +0100

    Adds polynomial time algorithm for Fibonacci numbers.

--- utilities/utilities.cpp
+++ utilities/utilities.cpp
@@ -10,3 +10,19 @@ double logarithmd(double x, double base) {
 	return log10(x) / log10(base);
 }
 
+long long int fibonacci(long long int n) {
+	if (n <= 0)
+		return 0;
+	if (n == 1)
+		return 1;
+
+	long long int fn0 = 0;
+	long long int fn1 = 1;
+	long long int fn = 0;
+	for (auto i = 2; i < n; ++i) {
+		fn = fn0 + fn1;
+		fn0 = fn1;
+		fn1 = fn;
+	}
+	return fn;
+}
--- utilities/utilities.h
+++ utilities/utilities.h
@@ -3,3 +3,5 @@
 float logarithmf(float x, float base = 10.f);
 
 double logarithmd(double x, double base = 10.);
+
+long long int fibonacci(long long int n);
