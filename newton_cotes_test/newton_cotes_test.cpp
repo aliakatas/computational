c0a35df356331bafe6fac70ac6581f99347c63d9
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-09-29 09:47:41 +0100

    Adds a trigonometric function for Newton-Cotes testing.

--- newton_cotes_test/newton_cotes_test.cpp
+++ newton_cotes_test/newton_cotes_test.cpp
@@ -3,49 +3,34 @@
 #include <iostream>
 #include <cmath>
 
-float polyf(float x) {
+float poly(float x) {
 	return 0.5f * pow(x, 4) + 6.f * x * x - 3.8f * x + 4.2f;
 }
 
-double polyd(double x) {
+double poly(double x) {
 	return 0.5 * pow(x, 4) + 6. * x * x - 3.8 * x + 4.2;
+}
+
+float trigon(float x) {
+	return cos(x / 2.f) - 3.f * x;
+}
+
+double trigon(double x) {
+	return cos(x / 2.) - 3. * x;
 }
 
 int main() {
 	
 	std::cout << "\n  Testing \"newton_cotes\" -->\n";
 	std::cout << " ********************** \n";
-	std::cout << "  Int_{-3, 3} 0.5 * x^4 + 6 * x^2 - 3.8 * x + 4.2  \n";
-	std::cout << " F ::  " << newton_cotesf(-3.f, 3.f, 50, polyf) << "  (exact: 181.8)\n";
-	std::cout << " D ::  " << newton_cotesd(-3., 3., 50, polyd) << "  (exact: 181.8)\n";
-	/*std::cout << " F :: log(e, e) = " << logarithmf(M_E, M_E) << "  (exp: 1.0)\n";
-	std::cout << " D :: log(e, e) = " << logarithmd(M_E, M_E) << "  (exp: 1.0)\n";
-	std::cout << " F :: log(98, 22) = " << logarithmf(98.f, 22.f) << "  (exp: 1.483307832)\n";
-	std::cout << " D :: log(98, 22) = " << logarithmd(98., 22.) << "  (exp: 1.483307832)\n";
-	std::cout << " F :: log(5, 11) = " << logarithmf(5.f, 11.f) << "  (exp: 0.6711877)\n";
-	std::cout << " D :: log(5, 11) = " << logarithmd(5., 11.) << "  (exp: 0.6711877)\n";*/
+	std::cout << "  Int_{-3, 3} 0.5 * x^4 + 6 * x^2 - 3.8 * x + 4.2  (50 segments) \n";
+	std::cout << " F ::  " << newton_cotesf(-3.f, 3.f, 50, poly) << "  (exact: 181.8)\n";
+	std::cout << " D ::  " << newton_cotesd(-3., 3., 50, poly) << "  (exact: 181.8)\n";
 	std::cout << " ********************** \n";
-	/*std::cout << " Fibonacci(0) = " << fibonacci(0) << "  (exp: 0)\n";
-	std::cout << " Fibonacci(1) = " << fibonacci(1) << "  (exp: 1)\n";
-	std::cout << " Fibonacci(13) = " << fibonacci(13) << "  (exp: 233)\n";
-	std::cout << " Fibonacci(-5) = " << fibonacci(-5) << "  (exp: -5)\n";
-	std::cout << " Fibonacci(100) = " << fibonacci(71) << "  (exp: 308061521170129)\n";*/
+	std::cout << "  Int_{-pi, pi} cos(x/2) - 3 * x  (50 segments) \n";
+	std::cout << " F ::  " << newton_cotesf(-M_PI, M_PI, 50, trigon) << "  (exact: 4.0)\n";
+	std::cout << " D ::  " << newton_cotesd(-M_PI, M_PI, 50, trigon) << "  (exact: 4.0)\n";
 	std::cout << " ********************** \n";
-	/*std::cout << " GCD(11, 3) = " << gcd(11, 3) << "  (exp: 1)\n";
-	std::cout << " GCD(25, 5) = " << gcd(25, 5) << "  (exp: 5)\n";
-	std::cout << " GCD(69, 23) = " << gcd(69, 23) << "  (exp: 23)\n";
-	std::cout << " GCD(9, 4) = " << gcd(9, 4) << "  (exp: 1)\n";
-	std::cout << " GCD(1234, 87) = " << gcd(1234, 87) << "  (exp: 1)\n";
-	std::cout << " GCD(7539, 36) = " << gcd(7539, 36) << "  (exp: 3)\n";*/
-	std::cout << " ********************** \n";
-	/*std::cout << " Is -5 prime? " << isPrime(-5) << "\n";
-	std::cout << " Is 0 prime? " << isPrime(0) << "\n";
-	std::cout << " Is 1 prime? " << isPrime(1) << "\n";
-	std::cout << " Is 2 prime? " << isPrime(2) << "\n";
-	std::cout << " Is 7 prime? " << isPrime(7) << "\n";
-	std::cout << " Is 15 prime? " << isPrime(15) << "\n";
-	std::cout << " Is 2971 prime? " << isPrime(2971) << " (it is...)\n";
-	std::cout << " Is 3001 prime? " << isPrime(3001) << " (it is...)\n";
-	std::cout << " Is 6858 prime? " << isPrime(6858) << "\n";*/
+
 	std::cout << "  <-- Done. \n";
 }
